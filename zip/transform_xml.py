4236b7ab33fba552902304cbda76bcaa7926de14
rhdekker <ronald.dekker@di.huc.knaw.nl>
2019-09-09 17:36:31 +0200

    Added Python script to transform XML file to HTML using XSLT.

---
+++ zip/transform_xml.py
@@ -0,0 +1,33 @@
+#!python
+# script to transform XML file to a HTML file using an XSLT file
+# created by Ronald
+
+import argparse
+import io
+
+from lxml import etree
+
+# TODO: "process some integers" should be changed into something meaningful
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument("xml_file", help="The XML file to parse and transform") # required=True
+parser.add_argument("xslt_file", help="The XSLT to transform the XML with")
+parser.add_argument("html_file", help="The file were the result of the transformation is stored")
+args = parser.parse_args()
+
+# print(args.xml_file)
+
+# read and parse the xslt file
+xslt_root = etree.parse(args.xslt_file)
+
+# treat it as an XSLT file
+transform = etree.XSLT(xslt_root)
+
+# read and parse the xml file
+doc = etree.parse(args.xml_file)
+
+# do the transformation
+result = transform(doc)
+
+# and write HTML file
+result.write_output(args.html_file)
